68409e7550d930ed4fd1301590ad4121f50b39fc
Abdelrahman <abdelrahman@abdo.abdo>
2024-09-23 11:50:08 +0200

    handle cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = { images: {
     domains: ['utfs.io'],
-  },};
+  },
+ async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'https://casecobra-phi-lyart.vercel.app/:path*',
+          },
+        ]
+      },};
 
 export default nextConfig;
